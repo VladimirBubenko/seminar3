82bf4db68ed0f354e50660b3fd3fbb24fad66911
Vladimir Bubenko <bawmc@mail.ru>
2023-02-07 21:30:08 +0700

    done task 25

---
+++ HW4/Program.cs
@@ -0,0 +1,21 @@
+﻿// HW4
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int rate(int a,int b)
+{
+    int result = 1;
+    int current = 1;
+    while (current <= b)
+    {
+        result = result * a;
+        current++;
+
+    }
+    return result;
+}
+
+Console.Write("Input a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rate(a,b));
