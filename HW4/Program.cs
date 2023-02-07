818809e3ed56044718103baaad64e8e5bc802f3d
Vladimir Bubenko <bawmc@mail.ru>
2023-02-07 21:54:56 +0700

    done Task 27

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,21 +1,44 @@
 ﻿// HW4
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-int rate(int a,int b)
-{
-    int result = 1;
-    int current = 1;
-    while (current <= b)
-    {
-        result = result * a;
-        current++;
+// int rate(int a,int b)
+// {
+//     int result = 1;
+//     int current = 1;
+//     while (current <= b)
+//     {
+//         result = result * a;
+//         current++;
 
-    }
-    return result;
-}
+//     }
+//     return result;
+// }
 
-Console.Write("Input a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input b: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(rate(a,b));
+// Console.Write("Input a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(rate(a,b));
+
+// Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// int sum(int number)
+// {
+//     int mult = 0;
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         mult = number%10;
+//         result = result + mult;
+//         number = number/10;
+//     }
+//     return result;
+// }
+
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(sum(number));
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+
