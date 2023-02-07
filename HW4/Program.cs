8237d81dda586eb31e0268ed4e9b6af67c6ba6d2
Vladimir Bubenko <bawmc@mail.ru>
2023-02-07 22:14:25 +0700

    done HW4

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -41,4 +41,25 @@
 
 // Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
 
+int[] mas(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input intdex {i} = ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
 
+void ShowArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Input a lengh of new array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = mas(size);
+ShowArray(myArray);
