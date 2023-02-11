fc3b00d568f3d592f6a446251f7087149b0f6c0f
Vladimir Bubenko <bawmc@mail.ru>
2023-02-11 17:05:44 +0700

    done HW5

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -44,28 +44,74 @@
 
 
 // Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-int[] CreateRandomArray(int size, int minValue, int maxValue)
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int [size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random ().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void ShowArray (int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         Console.Write(array[i] + " ");
+//     Console.WriteLine();
+// }
+// int sum(int[] array)
+// {
+//     int summary = 0;
+//     for (int i = 0; i < array.Length; i++)
+//         if(i%2!=0)
+//             summary = array[i] + summary;
+//     return summary;
+// }
+
+// Console.Write("Input a lengh of new array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a min of new array: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a max of new array: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, min, max);
+// ShowArray(myArray);
+
+// Console.Write($"Answer is {sum(myArray)}");
+
+// Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double [] CreateRandomArray(int size, int minValue, int maxValue)
 {
-    int[] array = new int [size];
+    double [] array = new double [size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random ().Next(minValue, maxValue + 1);
+        array[i] = new Random ().Next(minValue, maxValue + 1) + new Random().NextDouble();
     }
     return array;
 }
-void ShowArray (int[] array)
+
+void ShowArray (double[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
+        Console.Write(Math.Round(array[i],3) + " ");
     Console.WriteLine();
 }
-int sum(int[] array)
+
+double Result(double[] array)
 {
-    int summary = 0;
+    double max = array[0];
+    double min = array[0];
     for (int i = 0; i < array.Length; i++)
-        if(i%2!=0)
-            summary = array[i] + summary;
-    return summary;
+        if(array[i] > max)
+            max = array[i];
+
+    for (int i = 0; i < array.Length; i++)
+        if(array[i] < min)
+            min = array[i];
+
+    double result = max - min;
+    return Math.Round(result,3);
 }
 
 Console.Write("Input a lengh of new array: ");
@@ -75,8 +121,7 @@ int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a max of new array: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = CreateRandomArray(size, min, max);
+double [] myArray = CreateRandomArray(size, min, max);
 ShowArray(myArray);
-
-Console.Write($"Answer is {sum(myArray)}");
-
+double answer = Result(myArray);
+Console.WriteLine($"Answer is {answer}");
