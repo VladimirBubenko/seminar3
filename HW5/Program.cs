b66b86152fed6d6202e988ad5fd4ebaaec5fffc3
Vladimir Bubenko <bawmc@mail.ru>
2023-02-11 15:55:27 +0700

    start HW5

---
+++ HW5/Program.cs
@@ -0,0 +1,13 @@
+﻿// HW5. 
+// Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random ().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
